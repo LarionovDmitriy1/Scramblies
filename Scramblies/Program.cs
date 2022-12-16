54cc2dd507e8de9f6bcbd842f163c609059b8580
Студент1 <1@DESKTOP-7RO4MU5>
2022-12-16 12:54:22 +0300

    Добавьте файлы проекта.

---
+++ Scramblies/Program.cs
@@ -0,0 +1,4 @@
+﻿using Scramblies;
+
+var a = WorkScramblies.Scramble("scriptjavx", "javascript");
+Console.WriteLine(a);
---
+++ Scramblies/WorkScramblies.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scramblies;
+
+public class WorkScramblies
+{
+    public static bool Scramble(string str1, string str2)
+    {
+        int counter = 0;
+        for (int i = 0; i < str2.Length;)
+        {
+            for (int j = 0; j < str1.Length;)
+            {
+                if (counter != str2.Length && str1[j] == str2[i])
+                {
+                    counter++;
+                    i++;
+                    j = 0;
+                    continue;
+                }
+                j++;
+            }
+            if (counter != str2.Length)
+            {
+                return false;
+            }
+        }
+        if (counter == str2.Length)
+        {
+            return true;
+        }
+        return false;
+    }
+}
